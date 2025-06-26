692cdb51ddebd733f51737345a347d47548392f1
fellipetereska <fellipetereska@gmail.com>
2025-06-26 16:53:58 -0300

    att

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,2 +1,5 @@
-export const connect = "http://localhost:5268/api";
-export const connectPhoto = "http://localhost:5268";
+// export const connect = "http://localhost:5268/api";
+export const connect = "http://89.116.191.16:5005/api";
+
+// export const connectPhoto = "http://localhost:5268";
+export const connectPhoto = "http://89.116.191.16:5005";
