f1a637fef2bf3c980005fea8158f68f12177c71b
fellipetereska <fellipetereska@gmail.com>
2025-06-26 17:07:48 -0300

    att link

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,5 +1,5 @@
 // export const connect = "http://localhost:5268/api";
-export const connect = "http://apidoar.fabricadelaudos.com/api";
+export const connect = "https://apidoar.fabricadelaudos.com/api";
 
 // export const connectPhoto = "http://localhost:5268";
-export const connectPhoto = "http://apidoar.fabricadelaudos.com";
+export const connectPhoto = "https://apidoar.fabricadelaudos.com";
